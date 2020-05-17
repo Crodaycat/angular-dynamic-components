e0cde96bca46fc0d7b53d584bcb3a37931c0ac40
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-05-16 23:46:02 -0500

    REFACTOR: Code format and removing hard coded data

--- src/app/numeric-form/numeric-form.component.ts
+++ src/app/numeric-form/numeric-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-numeric-form',
   templateUrl: './numeric-form.component.html',
-  styleUrls: ['./numeric-form.component.css']
+  styleUrls: ['./numeric-form.component.css'],
 })
 export class NumericFormComponent implements OnInit {
+  controlLabel: string;
+  control: FormControl;
+  controlHit: string;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  ngOnInit() {}
 }
--- src/app/string-form/string-form.component.ts
+++ src/app/string-form/string-form.component.ts
@@ -1,21 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: "app-string-form",
-  templateUrl: "./string-form.component.html",
-  styleUrls: ["./string-form.component.css"],
+  selector: 'app-string-form',
+  templateUrl: './string-form.component.html',
+  styleUrls: ['./string-form.component.css'],
 })
 export class StringFormComponent implements OnInit {
   controlLabel: string;
   control: FormControl;
   controlHit: string;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.controlLabel = "Label de prueba";
-    this.control = this.formBuilder.control("hola", []);
-    this.controlHit = "Hint de prueba";
-  }
+  constructor() {}
 
   ngOnInit() {}
 }
